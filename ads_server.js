edc373f717eef11fbce075809cf75e14a25acc69
Avi Illouz <avi.illouz@gmail.com>
2019-07-25 22:42:35 +0300

    insert role and description from request

--- ads_server.js
+++ ads_server.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.use(bodyParser.json())
+
 
 var db = require('./mysql_client')
 
-app.post('/ad', (req, res) => {
 
-	db.query('insert into potato.ads (role, description) values ("a", "b");');
+app.post('/ad', (req, res) => {
+	console.log(req.body)
+	var body = req.body;
+
+	db.query(`insert into potato.ads (role, description) values ("${body.role}", "${body.description}");`);
 	res.send('add new ad');
 })
+
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
