cba4675da183d0c7f8bd5adc740e65bbe4c26bdc
Avi Illouz <avi.illouz@gmail.com>
2019-07-25 22:04:40 +0300

    connection to mysql with simple query

--- ads_server.js
+++ ads_server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.post('/ad', (req, res) => res.send('add new ad'))
+var db = require('./mysql_client')
+
+app.post('/ad', (req, res) => {
+
+	res.send('add new ad');
+	db.query('select * from potato.ads');
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
