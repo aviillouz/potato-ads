f9b5078aea2218cabdfa17faec0d704002f622b5
Avi Illouz <avi.illouz@gmail.com>
2019-07-28 21:47:22 +0300

    add auto id and timestamp

--- ads_table.sql
+++ ads_table.sql
@@ -2,10 +2,9 @@ ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY '1234';
 flush privileges;
 create database potato;
 create table potato.ads (
-	id int,
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	role text,
-	location text,
 	description text,
-	created_at datetime,
-	updated_at datetime
+	created_at datetime DEFAULT CURRENT_TIMESTAMP,
+	updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ); 
