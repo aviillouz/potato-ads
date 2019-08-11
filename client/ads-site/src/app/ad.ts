78d568db8c754a6a4127605d73ebb0bd19b5aec3
Avi Illouz <avi.illouz@gmail.com>
2019-08-11 17:54:14 +0300

    add ads table

---
+++ client/ads-site/src/app/ad.ts
@@ -0,0 +1,7 @@
+export class Ad {
+    id: number;
+    role: string;
+    description: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
--- client/ads-site/src/app/ads/ads.component.ts
+++ client/ads-site/src/app/ads/ads.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ad } from '../ad';
 
 @Component({
   selector: 'app-ads',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ads.component.css']
 })
 export class AdsComponent implements OnInit {
+
+  ads: Ad[] = [
+    { id: 1, role: "a", description: "b", createdAt: new Date(), updatedAt: new Date() },
+    { id: 2, role: "c", description: "d", createdAt: new Date(), updatedAt: new Date() }
+  ];
 
   constructor() { }
 
