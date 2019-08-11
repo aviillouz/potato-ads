dd6327eda69b6099f69478aca601b9bff6386557
Avi Illouz <avi.illouz@gmail.com>
2019-08-11 17:38:04 +0300

    add ads component skeleton

--- client/ads-site/src/app/app.module.ts
+++ client/ads-site/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdsComponent } from './ads/ads.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdsComponent
   ],
   imports: [
     BrowserModule,
